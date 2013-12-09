0a6c5efae87e15850513ca2373dfa76837f5927e
unknown <b3019229@CYPHER102.campus.ncl.ac.uk>
2013-12-09 17:41:44 +0000

    Signed-off-by: jiapeng Guo <b3019229@CYPHER102.campus.ncl.ac.uk>

---
+++ customers.php
@@ -0,0 +1,21 @@
+<html>
+	<body>
+		<form action="" method="post" name="customersInfo"  onsubmit="return CheckPost();">
+			<table>
+				<tr>
+					<td>user name:</td><td><input name="name" type="text"></td>
+					<td>user country:</td><td><input name="country" type="text"></td>
+				</tr>
+			</table>
+			<table width="914" border="2" align="center" cellpadding="1" cellspacing="1" bordercolor="#73BA08">
+				<tr align="center"> 
+					<td colspan="6">
+						<input type="submit" name="submit" value="Ìí¼Ó" />   
+						<input type="reset" name="reset" value="ÖØÐ´" />  |
+						<a href="add.php"><img src="back.jpg" border="0"></a> 
+					</td>
+				</tr>
+			</table>
+</form>
+	</body>
+</html>
